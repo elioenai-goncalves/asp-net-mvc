41ba51bf87ce5675a1b7ea8a841bdfcba2d59ca5
Elioenai.Gonçalves <elioenai.andrade@hotmail.com>
2024-09-12 23:33:56 -0300

    adicionando projeto demo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMVC.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+public DbSet<Aluno> Aluno { get; set; } = default!;
+}
